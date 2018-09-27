7c69bb5ddfc16ee4c01de69181c8c87377fee29d
Yangyunhui454545 <43237961+Yangyunhui454545@users.noreply.github.com>
2018-09-27 21:07:41 +0900

    Add files via upload

--- GAME/FirstPage.java
+++ GAME/FirstPage.java
@@ -1,20 +1,95 @@
-
+package game;
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;;
 
-public class FirstPage extends JFrame{
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;;
+
+//한정원:FirstPage관리자 페이지
+
+public class FirstPage{
 	private final int BUTTON_LOCATION_X = 480;
 	private final int BUTTON_LOCATION_Y = 400;
-	
+	static JFrame app;
 	JLabel timerLabel = new JLabel();
 	TimerThread th = new TimerThread(timerLabel);
+	Container c;
+	JButton button_gamestart = new JButton("게임 시작");
+	JButton button_exit = new JButton("나가기");
+	JButton button_setting = new JButton("ABCD");
+	
+	class work extends JPanel implements ActionListener {
+
+
+		private int HEIGHT = 500;
+		private int WIDTH = 700;
+		private int space;
+		private int speed;
+		private int width =80;
+		private int height =70;
+		private Rectangle human;
+		private Random random;
+		Timer t;
+		private ArrayList <Rectangle> ohuman;
+		
+		
+		public work() {
+			System.out.println("사각형");
+			t = new Timer(20,this);
+			random = new Random( ) ;
+			ohuman = new ArrayList<Rectangle>();
+			human = new Rectangle(WIDTH/2, HEIGHT-100, width, height);
+			space =300;
+			speed = 2;
+			addhuman(true);
+			t.start();
+		}
+		public void addhuman(boolean first) {
+			int positionx=random.nextInt() %2;
+			int x=0;
+			int y =0;
+			int Width =width;
+			int Height=height;
+			if(positionx==0) {
+				x = Width/2;
+				}
+			else {
+				x = Width/2+10;
+			}
+			if(first) {
+				ohuman.add(new Rectangle(x, y, Width, Height));
+			}
+			else {
+				ohuman.add(new Rectangle(x, x,Width, Height));
+			}
+		}
+		@Override
+		public void paintComponent(Graphics g) {
+			System.out.println("action");
+			super.paintComponent(g);
+			g.setColor(Color.MAGENTA);
+			for (Rectangle rect:(ohuman)) {
+				g.fillRect(rect.x, rect.y, rect.width, rect.height);
+			}
+		}
+		
+		
+		public void actionPerformed(ActionEvent arg0) {		
+			
+			Rectangle rect;
+			for (int i=0; i<ohuman.size(); i++) {
+				rect = ohuman.get(i);
+				rect.y += speed;
+			}
+		repaint();
+		}
+		
+	  }
 	
 	public FirstPage() {
-		
-		setTitle("FirstPage");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c = getContentPane();
+	
+		c = app.getContentPane();
 		c.setLayout(null);
 		c.setBackground(Color.WHITE);
 		
@@ -24,17 +99,17 @@ public class FirstPage extends JFrame{
 		timerLabel.setOpaque(true);
 		timerLabel.setBackground(Color.LIGHT_GRAY);
 		
-		JButton button_gamestart = new JButton("게임 시작");
+		
 		button_gamestart.setLocation(BUTTON_LOCATION_X,BUTTON_LOCATION_Y);
 		button_gamestart.setSize(240,40);
 		button_gamestart.addActionListener(new ActionListener_gamestart());
 		
-		JButton button_exit = new JButton("나가기");
+		
 		button_exit.setLocation(BUTTON_LOCATION_X,BUTTON_LOCATION_Y+100);
 		button_exit.setSize(240,40);
 		button_exit.addActionListener(new ActionListener_exit());
 		
-		JButton button_setting = new JButton("ABCD");		
+				
 		button_setting.setLocation(BUTTON_LOCATION_X,BUTTON_LOCATION_Y+200);
 		button_setting.setSize(240,40);
 		
@@ -45,15 +120,18 @@ public class FirstPage extends JFrame{
 		c.add(timerLabel);
 		
 
-		setSize(1200, 800); 
-		setVisible(true);
 		
 	}
 	
 	class ActionListener_gamestart implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			th.start();
 			
+			button_gamestart.setVisible(false);
+			button_exit.setVisible(false);
+			button_setting.setVisible(false);
+
+			new StageOne();
+
 		}
 	}	
 	class ActionListener_exit implements ActionListener{
@@ -62,15 +140,50 @@ public class FirstPage extends JFrame{
 		}
 	}
 	
+	class StageOne{
+		public StageOne() {
+			th.start();
+			app.getContentPane().remove(c);
+			work w = new work();
+			app.getContentPane().add(w);
+			app.revalidate();
+			app.repaint();
+	
+		}
+	}
+	
+	public void next_Stage(int stageNumber) {
+		switch(stageNumber) {
+		
+		//case 1 : new StageTwo();break;
+		//case 2 : new StageThree();break;
+		
+		}
+		
+	}
+	public void Clear() {
+		
+		JLabel clearLabel = new JLabel();
+		ImageIcon clearImg = new ImageIcon("dsklf.jpg");
+		clearLabel.setIcon(clearImg);
+		c.add(clearLabel);
+		clearLabel.setLocation(50,50);
+		
+	}
 
 
 
 	public static void main(String[] args) {
-		new FirstPage();
+			app = new JFrame();
+			app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			app.setVisible(true);
+			app.setSize(1200, 800); 
+			
+			new FirstPage();
 	}
 
 }
-	class TimerThread extends Thread{
+class TimerThread extends Thread{
 		private JLabel timerLabel;
 		
 		public TimerThread(JLabel timerLabel) {
@@ -79,10 +192,10 @@ public class FirstPage extends JFrame{
 		
 		@Override
 		public void run() {
-			double n=0;
-			while(n<=40) {
+			double n=40;
+			while(n>=0) {
 				timerLabel.setText(Double.toString(n));
-				n++;
+				n--;
 				try {
 					Thread.sleep(1000);
 				}
@@ -90,4 +203,5 @@ public class FirstPage extends JFrame{
 			}
 		}
 		
-	}
+}
+	
---
+++ GAME/Human.java
@@ -0,0 +1,76 @@
+package game;
+
+import java.awt.event.ActionListener;
+
+public class Human implements ActionListener {
+	private double time;
+	private int state;
+	private double speed;
+	
+	public Human() {
+		this.speed = 10;
+		this.state = 1;
+		this.time = 40;
+	}
+	
+	public void eating_Item(String item) {
+		
+		double game_Time = 0;
+		switch(item) {
+		case "water" :
+			game_Time +=3;
+			break;
+		case "wingShoes" :
+			speed+=5;
+			game_Time =2;
+			break;
+		case "clock" :
+			time +=3;
+			break;
+		}
+	}
+	public void eating_Obstacle(String item){
+		
+		double game_Time = 0;
+		switch(item) {
+		case "drunken" :
+			game_Time =1;
+			speed =0;
+			break;
+		case "cat" :
+			game_Time=0.5;
+			speed=0;
+			break;
+		case "grandma" :
+			game_Time =3;
+			speed =0;
+			break;
+		case "emergency" :
+			game_Time =2;
+			speed =0;
+			break;
+		case "trash" :
+			game_Time =0.5;
+			speed =0;
+			break;
+		case "manhole" :
+			state = 0;
+			break;
+		}
+		
+	}
+public void clear_Stage(int stage_Number) {
+	switch(stage_Number) {
+	case 1:
+		next_Stage(stage_Number);
+		break;
+	case 2:
+		next_Stage(stage_Number);
+		break;
+	case 3:
+		clear();
+		break;
+		}
+	}
+}
+
